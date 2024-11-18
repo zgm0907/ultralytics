ed697845a0099bb76ff619439cd071aab0ac15da
zgm0907 <13350125000@163.com>
2024-11-18 18:41:01 +0800

    Update tasks.py

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -10,6 +10,9 @@ from pathlib import Path
 import torch
 import torch.nn as nn
 
+
+
+from  ultralytics.nn.modules.PPA import *
 from ultralytics.nn.modules import (
     AIFI,
     C1,
@@ -999,6 +1002,7 @@ def parse_model(d, ch, verbose=True):  # model_dict, input_channels(3)
             PSA,
             SCDown,
             C2fCIB,
+            C3k2_PPA
         }:
             c1, c2 = ch[f], args[0]
             if c2 != nc:  # if c2 not equal to number of classes (i.e. for Classify() output)
@@ -1025,6 +1029,7 @@ def parse_model(d, ch, verbose=True):  # model_dict, input_channels(3)
                 C2fPSA,
                 C2fCIB,
                 C2PSA,
+                C3k2_PPA
             }:
                 args.insert(2, n)  # number of repeats
                 n = 1
