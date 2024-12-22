a188010d2991861409698320ad7dd78ba2d7b294
zgm0907 <13350125000@163.com>
2024-12-22 16:35:53 +0800

    Create CAA.py

---
+++ ultralytics/nn/attention/CAA.py
@@ -0,0 +1,37 @@
+import torch
+import torch.nn as nn
+from mmcv.cnn import ConvModule
+from mmengine.model import BaseModule
+from typing import Optional, Union, Sequence
+
+
+ 
+class CAA(BaseModule):
+    """Context Anchor Attention"""
+    def __init__(
+            self,
+            channels: int,
+            h_kernel_size: int = 11,
+            v_kernel_size: int = 11,
+            norm_cfg: Optional[dict] = dict(type='BN', momentum=0.03, eps=0.001),
+            act_cfg: Optional[dict] = dict(type='SiLU'),
+            init_cfg: Optional[dict] = None,
+    ):
+        super().__init__(init_cfg)
+        self.avg_pool = nn.AvgPool2d(7, 1, 3)
+        self.conv1 = ConvModule(channels, channels, 1, 1, 0,
+                                norm_cfg=norm_cfg, act_cfg=act_cfg)
+        self.h_conv = ConvModule(channels, channels, (1, h_kernel_size), 1,
+                                 (0, h_kernel_size // 2), groups=channels,
+                                 norm_cfg=None, act_cfg=None)
+        self.v_conv = ConvModule(channels, channels, (v_kernel_size, 1), 1,
+                                 (v_kernel_size // 2, 0), groups=channels,
+                                 norm_cfg=None, act_cfg=None)
+        self.conv2 = ConvModule(channels, channels, 1, 1, 0,
+                                norm_cfg=norm_cfg, act_cfg=act_cfg)
+        self.act = nn.Sigmoid()
+
+    def forward(self, x):
+        attn_factor = self.act(self.conv2(self.v_conv(self.h_conv(self.conv1(self.avg_pool(x))))))
+        return attn_factor
+ 
