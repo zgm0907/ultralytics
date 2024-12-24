d33e49d150020e5d2a564a13b51045888c3e9ed2
zgm0907 <13350125000@163.com>
2024-12-24 10:03:38 +0800

    Update head.py

--- ultralytics/nn/modules/head.py
+++ ultralytics/nn/modules/head.py
@@ -57,7 +57,7 @@ class Detect(nn.Module):
                 for x in ch
             )
         )
-   
+        self.stem = nn.ModuleList(nn.Sequential(ScConv(x), Conv(x, x, 1)) for x in ch)
         self.dfl = DFL(self.reg_max) if self.reg_max > 1 else nn.Identity()
 
         if self.end2end:
@@ -70,7 +70,7 @@ class Detect(nn.Module):
             return self.forward_end2end(x)
 
         for i in range(self.nl):
-           
+            x[i] = self.stem[i](x[i])
             x[i] = torch.cat((self.cv2[i](x[i]), self.cv3[i](x[i])), 1)
         if self.training:  # Training path
             return x
@@ -175,7 +175,131 @@ class Detect(nn.Module):
         i = torch.arange(batch_size)[..., None]  # batch indices
         return torch.cat([boxes[i, index // nc], scores[..., None], (index % nc)[..., None].float()], dim=-1)
         
-
+###################### ScConv  ####     START  #############################
+ 
+ 
+import torch.nn.functional as F
+from torch.nn.init import constant_, xavier_uniform_
+ 
+class GroupBatchnorm2d(nn.Module):
+    def __init__(self, c_num: int,
+                 group_num: int = 16,
+                 eps: float = 1e-10
+                 ):
+        super(GroupBatchnorm2d, self).__init__()
+        assert c_num >= group_num
+        self.group_num = group_num
+        self.gamma = nn.Parameter(torch.randn(c_num, 1, 1))
+        self.beta = nn.Parameter(torch.zeros(c_num, 1, 1))
+        self.eps = eps
+ 
+    def forward(self, x):
+        N, C, H, W = x.size()
+        x = x.view(N, self.group_num, -1)
+        mean = x.mean(dim=2, keepdim=True)
+        std = x.std(dim=2, keepdim=True)
+        x = (x - mean) / (std + self.eps)
+        x = x.view(N, C, H, W)
+        return x * self.gamma + self.beta
+ 
+ 
+class SRU(nn.Module):
+    def __init__(self,
+                 oup_channels: int,
+                 group_num: int = 16,
+                 gate_treshold: float = 0.5
+                 ):
+        super().__init__()
+ 
+        self.gn = GroupBatchnorm2d(oup_channels, group_num=group_num)
+        self.gate_treshold = gate_treshold
+        self.sigomid = nn.Sigmoid()
+ 
+    def forward(self, x):
+        gn_x = self.gn(x)
+        w_gamma = self.gn.gamma / sum(self.gn.gamma)
+        reweigts = self.sigomid(gn_x * w_gamma)
+        # Gate
+        info_mask = reweigts >= self.gate_treshold
+        noninfo_mask = reweigts < self.gate_treshold
+        x_1 = info_mask * x
+        x_2 = noninfo_mask * x
+        x = self.reconstruct(x_1, x_2)
+        return x
+ 
+    def reconstruct(self, x_1, x_2):
+        x_11, x_12 = torch.split(x_1, x_1.size(1) // 2, dim=1)
+        x_21, x_22 = torch.split(x_2, x_2.size(1) // 2, dim=1)
+        return torch.cat([x_11 + x_22, x_12 + x_21], dim=1)
+ 
+ 
+class CRU(nn.Module):
+    '''
+    alpha: 0<alpha<1
+    '''
+ 
+    def __init__(self,
+                 op_channel: int,
+                 alpha: float = 1 / 2,
+                 squeeze_radio: int = 2,
+                 group_size: int = 2,
+                 group_kernel_size: int = 3,
+                 ):
+        super().__init__()
+        self.up_channel = up_channel = int(alpha * op_channel)
+        self.low_channel = low_channel = op_channel - up_channel
+        self.squeeze1 = nn.Conv2d(up_channel, up_channel // squeeze_radio, kernel_size=1, bias=False)
+        self.squeeze2 = nn.Conv2d(low_channel, low_channel // squeeze_radio, kernel_size=1, bias=False)
+        # up
+        self.GWC = nn.Conv2d(up_channel // squeeze_radio, op_channel, kernel_size=group_kernel_size, stride=1,
+                             padding=group_kernel_size // 2, groups=group_size)
+        self.PWC1 = nn.Conv2d(up_channel // squeeze_radio, op_channel, kernel_size=1, bias=False)
+        # low
+        self.PWC2 = nn.Conv2d(low_channel // squeeze_radio, op_channel - low_channel // squeeze_radio, kernel_size=1,
+                              bias=False)
+        self.advavg = nn.AdaptiveAvgPool2d(1)
+ 
+    def forward(self, x):
+        # Split
+        up, low = torch.split(x, [self.up_channel, self.low_channel], dim=1)
+        up, low = self.squeeze1(up), self.squeeze2(low)
+        # Transform
+        Y1 = self.GWC(up) + self.PWC1(up)
+        Y2 = torch.cat([self.PWC2(low), low], dim=1)
+        # Fuse
+        out = torch.cat([Y1, Y2], dim=1)
+        out = F.softmax(self.advavg(out), dim=1) * out
+        out1, out2 = torch.split(out, out.size(1) // 2, dim=1)
+        return out1 + out2
+ 
+ 
+class ScConv(nn.Module):
+    # https://github.com/cheng-haha/ScConv/blob/main/ScConv.py
+    def __init__(self,
+                 op_channel: int,
+                 group_num: int = 16,
+                 gate_treshold: float = 0.5,
+                 alpha: float = 1 / 2,
+                 squeeze_radio: int = 2,
+                 group_size: int = 2,
+                 group_kernel_size: int = 3,
+                 ):
+        super().__init__()
+        self.SRU = SRU(op_channel,
+                       group_num=group_num,
+                       gate_treshold=gate_treshold)
+        self.CRU = CRU(op_channel,
+                       alpha=alpha,
+                       squeeze_radio=squeeze_radio,
+                       group_size=group_size,
+                       group_kernel_size=group_kernel_size)
+ 
+    def forward(self, x):
+        x = self.SRU(x)
+        x = self.CRU(x)
+        return x
+ 
+###################### ScConv  ####     END  #############################
 
 class Segment(Detect):
     """YOLO Segment head for segmentation models."""
